2a60c778d14a24fecb95932902e00c26f5d9e764
Tabasco97 <giuseppe.caci97@libero.it>
2024-09-02 13:46:14 +0200

    testati pre/post processori e moduli di testing, ambiente pronto al develop

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,19 @@
-import { defineConfig } from 'cypress';
+import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // Implementa ascoltatori di eventi del nodo qui se necessario
     },
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}', // Percorso dei tuoi file di test
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}", // Percorso dei tuoi file di test
     viewportWidth: 1000, // Larghezza della finestra del browser per i test
     viewportHeight: 660, // Altezza della finestra del browser per i test
+  },
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
   },
 });
