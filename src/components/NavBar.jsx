48d3d0c4d91b77b22ffa4c89cb8bfe0de2e710a3
Tabasco97 <giuseppe.caci97@libero.it>
2024-09-03 23:17:07 +0200

    menu hamburger per mobile/tablet ampliato

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,63 @@
 import React from "react";
+import { useState,useEffect } from "react";
 import { Link } from "react-router-dom";
 import ThemeToggle from "./ThemeToggle";
 import LanguageSwitcher from "./LenguageSwitcher";
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.classList.add('no-scroll');
+    } else {
+      document.body.classList.remove('no-scroll');
+    }
+  }, [isOpen]);
+
   return (
     <>
-      <div className="header">
+      <div className="hamburger-menu">
+        <div
+          className={`hamburger-icon ${isOpen ? "open" : ""}`}
+          onClick={toggleMenu}
+        >
+          <div className="hamburger-line"></div>
+          <div className="hamburger-line"></div>
+          <div className="hamburger-line"></div>
+        </div>
+        <nav className={`menu ${isOpen ? "open" : ""}`}>
+          <ul>
+            <li>
+              <Link to="/" onClick={toggleMenu}>Home</Link >
+            </li>
+            <li>
+              <Link to="/about" onClick={toggleMenu}>About</Link>
+            </li>
+            <li>
+              <Link to="/contact" onClick={toggleMenu}>Contact</Link>
+            </li>
+            <li>
+              <Link to="/blog" onClick={toggleMenu}>Blog</Link>
+            </li>
+            <li>
+              <Link to="/portfolio" onClick={toggleMenu}>Portfolio</Link>
+            </li>
+            <li>
+            <ThemeToggle/>
+            </li>
+            <li>
+            <LanguageSwitcher/>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+       <div className="header">
         <nav>
           <ul>
             <li>
