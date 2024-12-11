e8c8e7ac27c5af3c1bfb2586eb8d0131b5980cbf
Tabasco97 <giuseppe.caci97@libero.it>
2024-12-11 20:16:57 +0100

    prova fix 1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,6 @@ import { useContext } from "react";
 import ThemeContext from "../store/theme/ThemeContext";
 //icone
 import GitHubIcon from '@mui/icons-material/GitHub';
-import InstagramIcon from '@mui/icons-material/Instagram';
 import EmailIcon from '@mui/icons-material/Email';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
