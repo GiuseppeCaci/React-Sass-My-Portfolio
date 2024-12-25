80350c050432b5b0fb206fd8fd09eb474a476d28
Tabasco97 <giuseppe.caci97@libero.it>
2024-12-25 19:25:18 +0100

    ampliata animazione loading in chatbox

---
+++ src/components/ChatBoxComponent/LoadingBalls.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const LoadingBalls = () => {
+
+    return(
+      <>
+         <div  className="chat-loading">
+             <p className="slide-top">•</p>
+             <p className="slide-bottom">•</p>
+             <p className="slide-top">•</p>
+            </div>
+      </>
+    )
+}
+
+export default LoadingBalls
--- src/pages/ContattiChatBox.jsx
+++ src/pages/ContattiChatBox.jsx
@@ -1,14 +1,14 @@
 import React, { useEffect, useState, useRef, memo, useContext } from "react";
 import { Link } from "react-router-dom";
-import { motion } from "framer-motion";
 import ThemeContext from "../store/theme/ThemeContext";
 import useVisibilityAndScrollReset from "../components/UseHooks/useVisibilityAndScrollReset";
 import { useTranslation } from "react-i18next";
 import { gsap } from "gsap";
 import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import LoadingBalls from "../components/ChatBoxComponent/LoadingBalls";
 
 const ContattiChatBox = () => {
-  const { t } = useTranslation('chatBox');
+  const { t } = useTranslation("chatBox");
   const isVisible = useVisibilityAndScrollReset();
   gsap.registerPlugin(ScrollToPlugin);
   const { theme } = useContext(ThemeContext);
@@ -17,13 +17,7 @@ const ContattiChatBox = () => {
   ]);
   const [isLoading, setIsLoading] = useState(false);
 
-  const predefinedQuestions = t('predefinedQuestions', { returnObjects: true }); // Ottieni le domande dal file JSON
-  const citazioni = t('quotes', { returnObjects: true }); // Ottieni le citazioni dal file JSON
-
-  const getRandomQuote = () => {
-    const randomIndex = Math.floor(Math.random() * citazioni.length);
-    return citazioni[randomIndex];
-  };
+  const predefinedQuestions = t("predefinedQuestions", { returnObjects: true }); // Ottieni le domande dal file JSON
 
   const messagesEndRef = useRef(null);
 
@@ -38,16 +32,13 @@ const ContattiChatBox = () => {
 
     setMessages((prevMessages) => [
       ...prevMessages,
-      { sender: "bot", text: ['💬...'], buttons: [], textButtons: [] },
+      { sender: "bot", text: ["loading"], buttons: [], textButtons: [] },
     ]);
     setIsLoading(true);
 
     setTimeout(() => {
       const botMessage = generateBotResponse(question);
-      setMessages((prevMessages) => [
-        ...prevMessages.slice(0, -1),
-        botMessage,
-      ]);
+      setMessages((prevMessages) => [...prevMessages.slice(0, -1), botMessage]);
       setIsLoading(false);
     }, 1000);
   };
@@ -58,59 +49,67 @@ const ContattiChatBox = () => {
     let textButtonQuestion = [];
 
     switch (question) {
-        case t('responses.webAppCreation.question'):
-          response = [t('responses.webAppCreation.response')];
-          textButtonQuestion = [t('responses.notListenMusicFunky.question'), t('responses.favoriteQuote.question')];
-          break;
-          case t('responses.notListenMusicFunky.question'):
-            response = [t('responses.notListenMusicFunky.response')];
-            break;
-            case t('responses.favoriteQuote.question'):
-              response = [t('responses.favoriteQuote.response')];
-              break;
-
-      case t('responses.greet.question'):
-        response = [t('responses.greet.response')];
-        textButtonQuestion = [ t('responses.webAppCreation.question')];
+      case t("responses.webAppCreation.question"):
+        response = [t("responses.webAppCreation.response")];
+        textButtonQuestion = [
+          t("responses.notListenMusicFunky.question"),
+          t("responses.favoriteQuote.question"),
+        ];
+        break;
+      case t("responses.notListenMusicFunky.question"):
+        response = [t("responses.notListenMusicFunky.response")];
+        break;
+      case t("responses.favoriteQuote.question"):
+        response = [t("responses.favoriteQuote.response")];
         break;
 
-      case t('responses.chooseGiuseppe.question'):
-        response = [t('responses.chooseGiuseppe.response')];
+      case t("responses.greet.question"):
+        response = [t("responses.greet.response")];
+        textButtonQuestion = [t("responses.webAppCreation.question")];
         break;
 
-      case t('responses.portfolio.question'):
-        response = [t('responses.portfolio.response')];
-        buttons = [t('buttons.GoPortfolio'), "/portfolio"];
-        textButtonQuestion = [t('buttons.portfolioDetails')];
+      case t("responses.chooseGiuseppe.question"):
+        response = [t("responses.chooseGiuseppe.response")];
         break;
 
-      case t('buttons.portfolioDetails'):
-        response = [t('responses.portfolioMoreDetails.response')];
+      case t("responses.portfolio.question"):
+        response = [t("responses.portfolio.response")];
+        buttons = [t("buttons.GoPortfolio"), "/portfolio"];
+        textButtonQuestion = [t("buttons.portfolioDetails")];
         break;
 
-      case t('responses.aboutMe.question'):
-        response = [t('responses.aboutMe.response')];
-        buttons = [t('buttons.AboutMe'), "/about"];
-        textButtonQuestion = [t('responses.myBlog.question')];
+      case t("buttons.portfolioDetails"):
+        response = [t("responses.portfolioMoreDetails.response")];
         break;
 
-      case t('responses.myBlog.question'):
-        response = [t('responses.myBlog.response')];
-        buttons = [t('buttons.myBlog'), "https://adventurescode.com/"];
+      case t("responses.aboutMe.question"):
+        response = [t("responses.aboutMe.response")];
+        buttons = [t("buttons.AboutMe"), "/about"];
+        textButtonQuestion = [t("responses.myBlog.question")];
         break;
 
-      case t('responses.contactMe.question'):
-        response = [t('responses.contactMe.response')];
-        buttons = [t('buttons.contactMe'), `mailto:${import.meta.env.VITE_EMAIL}`];
+      case t("responses.myBlog.question"):
+        response = [t("responses.myBlog.response")];
+        buttons = [t("buttons.myBlog"), "https://adventurescode.com/"];
         break;
 
-      case t('messages.buttonBackToQuestions'):
-        response = [t('messages.chatbotDescription')];
+      case t("responses.contactMe.question"):
+        response = [t("responses.contactMe.response")];
+        buttons = [
+          t("buttons.contactMe"),
+          `mailto:${import.meta.env.VITE_EMAIL}`,
+        ];
+        break;
+
+      case t("messages.buttonBackToQuestions"):
+        response = [t("messages.chatbotDescription")];
         textButtonQuestion = predefinedQuestions;
         break;
 
       default:
-        response = [t('messages.defaultResponse') || "Non ho capito, puoi ripetere?"];
+        response = [
+          t("messages.defaultResponse") || "Non ho capito, puoi ripetere?",
+        ];
     }
     return {
       sender: "bot",
@@ -124,7 +123,7 @@ const ContattiChatBox = () => {
     if (messages.length > 1) {
       const messagesEnd = messagesEndRef.current;
       const offset = 400; // Definisci l'offset (puoi modificarlo a piacere)
-  
+
       gsap.to(window, {
         scrollTo: { y: messagesEnd.offsetTop - offset, autoKill: false },
         duration: 2, // Tempo in secondi per il movimento
@@ -135,27 +134,41 @@ const ContattiChatBox = () => {
 
   const Message = memo(({ msg, theme, handleUseMessage }) => {
     const [isVisible, setIsVisible] = useState(false);
-  
+
     useEffect(() => {
       // Fai apparire il messaggio dopo un breve ritardo
       const timer = setTimeout(() => {
         setIsVisible(true);
       }, 100); // Ritardo di 100ms per far partire la transizione
-  
+
       return () => clearTimeout(timer);
     }, []); // Effetto da eseguire una sola volta quando il messaggio viene montato
-  
+
     return (
       <div
         style={{
           textAlign: msg.sender === "user" ? "right" : "left",
         }}
-        className={`${theme} ${msg.sender === "user" ? "primary chat-user" : "secondary chat-pc"} ${msg.text[0] === '💬...' ? "pc-write" : null} message ${isVisible ? 'message-visible' : ''}`}
+        className={`${theme} ${
+          msg.sender === "user" ? "primary chat-user" : "secondary chat-pc"
+        } ${msg.text[0] === "loading" ? "pc-write" : null} message ${
+          isVisible ? "message-visible" : ""
+        }`}
       >
-        {msg.text.map((text, index) => (
-          <p key={index} dangerouslySetInnerHTML={{ __html: text }}></p>
-        ))}
-  
+        {msg.text.map((text, index) =>
+          msg.text[0] === "loading" ? (
+            <div key={index}>
+           <LoadingBalls/>
+           </div>
+          ) : (
+            <p
+              key={index}
+              dangerouslySetInnerHTML={{ __html: text }}
+              className=""
+            />
+          )
+        )}
+
         <div className="button-chat">
           {msg.buttons.length > 0 && (
             <Link to={`${msg.buttons[1]}`}>
@@ -164,20 +177,29 @@ const ContattiChatBox = () => {
           )}
           {msg.textButtons.length > 0 &&
             msg.textButtons.map((questionExtra, index) => (
-              <button key={index} onClick={() => handleUseMessage(questionExtra)}>
+              <button
+                key={index}
+                onClick={() => handleUseMessage(questionExtra)}
+              >
                 {questionExtra}
               </button>
             ))}
-          {msg.textButtons.length === 0 && msg.sender === "bot" && !isLoading && (
-            <button onClick={() => handleUseMessage(t('messages.buttonBackToQuestions'))}>
-              {t('cta-1')}
-            </button>
-          )}
+          {msg.textButtons.length === 0 &&
+            msg.sender === "bot" &&
+            !isLoading && (
+              <button
+                onClick={() =>
+                  handleUseMessage(t("messages.buttonBackToQuestions"))
+                }
+              >
+                {t("cta-1")}
+              </button>
+            )}
         </div>
       </div>
     );
   });
-  
+
   return (
     <div
       className={`main-base-chat chatBox-base ${theme} primary container-invisible ${
@@ -185,8 +207,8 @@ const ContattiChatBox = () => {
       }`}
     >
       <div className={`chatBox-messages`}>
-        <h3>{t('messages.chatbotIntro')}</h3>
-        <p>{t('messages.chatbotDescription')}</p>
+        <h3>{t("messages.chatbotIntro")}</h3>
+        <p>{t("messages.chatbotDescription")}</p>
         {predefinedQuestions.map((element, index) => (
           <button key={index} onClick={() => handleUseMessage(element)}>
             {element}
@@ -207,8 +229,7 @@ const ContattiChatBox = () => {
           ))}
         <div ref={messagesEndRef} />
       </div>
-      <div className="more-margin-bottom">
-      </div>
+      <div className="more-margin-bottom"></div>
     </div>
   );
 };
